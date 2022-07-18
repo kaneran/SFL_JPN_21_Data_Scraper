33615874a0462ca23fc38a8e5537c8ecad8857d1
Kane <95705546+kaneran@users.noreply.github.com>
2022-07-18 22:15:45 +0100

    Extrated partial data for each match result row

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.Playwright;
+using SFL_JPN;
 using System.Text.RegularExpressions;
 
 using var playright = await Playwright.CreateAsync();
@@ -18,5 +19,23 @@ var matchResultsData = data.Where(data => data.Contains("December") || data.Cont
 
 matchResultsData.ForEach((matchResult) =>
 {
-    var matchResults = matchResult.Split("\n").Where(data => data.Contains("Away:") || data.Contains("Home:"));
+    var matchResults = matchResult.Split("\n").Where(data => data.Contains("Away:") || data.Contains("Home:")).ToList();
+    var resultId = 0;
+    matchResults.ForEach(result =>
+    {
+        var matchesWonByPlayers = result.Substring(result.Length - 4).Replace(".","").Split("-");
+        var numberOfMatchesWonByWinningPlayer = matchesWonByPlayers[0];
+        var numberOfMatchesLostByLosingPlayer = matchesWonByPlayers[1];
+
+
+        //Player name, Character used, matches won, match result (W, L) , result id
+        var playerDataStrings = result.Replace("•","").Split(new string[] { "beat" }, StringSplitOptions.None);
+        for(var i = 0; i < playerDataStrings.Length; i++)
+        {
+            var playerData = playerDataStrings[i].Split(" ").Where(data => !string.IsNullOrEmpty(data)).ToList();
+            var matchResult = new Result() { ResultId = resultId };
+        }
+        resultId++;
+        Console.WriteLine();
+    });
 });
---
+++ Result.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFL_JPN
+{
+    public class Result
+    {
+        public int ResultId { get; set; }
+        public string Player { get; set; }
+        public string Character { get; set; }
+        public int MatchesWon { get; set; }
+    }
+}
